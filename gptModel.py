071dbb79c31e09186eaa3eb51d3909e4d6ef2eb5
Salman Khan <salkhan23@gmail.com>
2023-11-26 14:26:11 -0500

    Rework single attention head and multiheaded attention. Major changes include moving the tril to the initialization function and using it properly in the forward function.  This also requiered adding the max context length  as an input paramter. TODO Ali Ghodsi's way of adding masking

--- gptModel.py
+++ gptModel.py
@@ -47,101 +47,101 @@ def print_model_parameters(m):
     print("\nTotal Number of Parameters {}, Trainable {}, Fixed {}".format(n_train_p + n_fixed_p, n_train_p, n_fixed_p))
 
 
-class AttentionSingleHead(nn.Module):
-    def __init__(self, embed_dim, head_dim, masked=False):
+class AttentionHead(nn.Module):
+    def __init__(self, embed_dim, head_dim, max_context_len, causal=False):
         """
-        Single Attention head
-        :param embed_dim: input data  dimension
-        :param head_dim: output data dimension (or internal dimensionality)
-        :param masked: (default = False). Where to use causal mask. @ each time step,  cannot use future values.
+
+        :param embed_dim: input embeddings dimensionality
+        :param head_dim: output dimensionality of head
+        :params max_context_len: max length of context window
+        :param causal: whether inputs can look at future time steps or not. Default = True
         """
         super().__init__()
+        self.causal = causal
         self.embed_dim = embed_dim
         self.head_dim = head_dim
-        self.masked=masked
 
-        # Define the layers
-        self.query = nn.Linear(embed_dim, head_dim, bias=False)
+        # Layers
         self.key = nn.Linear(embed_dim, head_dim, bias=False)
+        self.query = nn.Linear(embed_dim, head_dim, bias=False)
         self.value = nn.Linear(embed_dim, head_dim, bias=False)
+
+        # for masked attention
+        if causal:
+            self.register_buffer('tril', torch.tril(torch.ones((max_context_len, max_context_len)))),  # [T, T]
 
     def forward(self, x_in):
         """
-        :param x_in: [B,T, ch]  ch = embed_dim
-        :return: y: [B, T, head_dim]
+        :param x_in: [B,T, embed_dim] matrix of indices
         """
+        b, t, ch = x_in.shape
 
         q = self.query(x_in)  # [B,T, head_dim]
-        k = self.key(x_in)    # [B,T, head_dim]
-        v = self.value(x_in)  # [B,T, head_dim]
+        k = self.key(x_in)  # [B, T, head_dim]
+        v = self.value(x_in)  # [B,T,head_dim]
 
-        # scaled_dot_product score/attention
-        s = q @ torch.transpose(k, 2, 1) / self.head_dim**0.5
-        # transpose [B,T,ch] -> [B,ch,T]
-        # matrix multiply: [B, T, head_dim]*[B ,head_dim, T] = [B, T, T]
-        if self.masked:
-            b, t, ch = x_in.shape
+        s = q @ torch.transpose(k, 2, 1) / self.head_dim**0.5  # [B,T, head_dim] * [B,head_dim, T]= [B, T, T]
 
-            # Ali Ghodsi way
+        if self.causal:
+            # Ali Ghodsi way: softmax(k.T @ v + m)
             # Mask should be  [0 -inf, -inf]
             #                 [0,   0, -inf]
             #                 [0,   0,    0]
-            tril = torch.triu(torch.ones(t, t))
-            m = torch.zeros((t, t))
-            m.masked_fill(tril == 1, float('-inf'))
-            s = s + m
+            # TODO: add Ali Ghodsi way
 
-            # # Andrej Karpathy way
+            # Andrej Karpathy way
             # Mask [1,-inf, -inf]
             #      [1,   1, -inf]
             #      [1,   1,    1]
-            tril = torch.tril(torch.ones(t, t))
-            s = s.masked_fill(tril == 0, float('-inf'))
+            mask = self.tril[0:t, 0:t]
+            s = s.masked_fill(mask == 0, float("-inf"))
 
-        a = F.softmax(s, dim=1)  # [B, T, T]
-        # TODO: Performance and better generated text if softmax over second dimension vs. last.
+        a = F.softmax(s, dim=1)  # [B, head_dim, T] @ [B, T, T] = [B, head_dim, T]
+        # TODO: Better Performance and generated text if softmax over second dimension vs. last.
         #  Both are T Why is one better than the other?
 
-        y = a @ v  # [B,T,T] * [B,T,head_dim] = [B,T,head_dim]
+        z = a @ v  # [B, T, T] @ [B, T, head_dim]   [B, T, head_dim]
 
-        # Note the difference between most notes and this implementation,
-        # due to PyTorch's different way of storing mats
+        # Note diff b/w notes and this implementation. Due to PyTorch's different way of storing mats
 
-        return y
+        return z
 
 
 class MultiHeadedAttention(nn.Module):
-    def __init__(self, embed_dim, n_heads, masked=False):
+    def __init__(self, embed_dim, n_heads, max_context_len, causal=False):
         """
-        Multi-headed attention
+
         :param embed_dim:
         :param n_heads:
-        :param masked (Default = False)
+        :param max_context_len:
+        :param causal:
         """
+
         super().__init__()
         self.embed_dim = embed_dim
         self.n_heads = n_heads
+        self.causal = causal
+        self.max_context_len = max_context_len
 
-        # Checks on embedded_dim and num_attention_heads checks
         if embed_dim % n_heads != 0:
             raise Exception(
                 "Embedded dimension ({}) should be perfectly divisible by num attention heads ({})".format(
                     embed_dim, n_heads))
+        self.single_head_dim = embed_dim // n_heads
 
-        self.attn_head_dim = torch.tensor(embed_dim / n_heads, dtype=torch.int)
-
-        # Declare the layers
-        self.attention_layers = nn.ModuleList(
-            AttentionSingleHead(embed_dim, head_dim=self.attn_head_dim, masked=masked) for _ in range(n_heads))
+        # layers ---------
+        self.attention_heads = nn.ModuleList(
+            AttentionHead(embed_dim=embed_dim, head_dim=self.single_head_dim, causal=causal,
+                          max_context_len=max_context_len) for _ in range(n_heads))
 
     def forward(self, x_in):
         """
 
-        :param x_in:  [B,T, embed_dim]
+        :param x_in: [B,T, embed_dim]
         :return:
         """
-        y = torch.concat([attn_head(x_in) for attn_head in self.attention_layers], dim=2)  # concatenate on embed_dim
-        return y
+        z = torch.concat([head(x_in) for head in self.attention_heads], dim=2)  # concatenate on embed dim
+        return z
 
 
 class GptModel(nn.Module):
@@ -161,7 +161,7 @@ class GptModel(nn.Module):
         # Declare the layers  ----------------------------------------------------------------
 
         # Word/ Symbol Embeddings
-        # self.embedding_table.weight = [vocab_s, embed_dim] mat. Each Row is a separate vocab word
+        # self.embedding_table.weight = [vocab_s, embed_dim] mat. Each row is a separate vocab word
         self.token_embed = nn.Embedding(num_embeddings=vocab_s, embedding_dim=embed_dim)
 
         # Positional embeddings
@@ -174,7 +174,8 @@ class GptModel(nn.Module):
         self.register_buffer('pos_v', torch.arange(block_s))
 
         # Multi-headed attention layer
-        self.self_attention = MultiHeadedAttention(embed_dim, n_attn_heads)
+        self.self_attention = \
+            MultiHeadedAttention(embed_dim=embed_dim, n_heads=n_attn_heads, causal=True, max_context_len=block_s)
 
         self.layer_norm = nn.LayerNorm(embed_dim)
 
